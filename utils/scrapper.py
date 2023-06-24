2eb841857b27e2cfdff92f485ec33d71bee10d44
Akmal Hameed <akmal.hameed@hiacuity.com>
2023-06-24 16:05:56 +0530

    workflow patch

--- utils/scrapper.py
+++ utils/scrapper.py
@@ -40,8 +40,6 @@ class Scraper:
     def setup_driver_options(self):
         self.driver_options = ChromeOptions()
 
-        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-        self.driver_options.add_argument(f'user-agent={user_agent}')
         self.driver_options.add_argument('--no-sandbox')
         self.driver_options.add_argument('--window-size=1920,1080')
         self.driver_options.add_argument('--headless')
