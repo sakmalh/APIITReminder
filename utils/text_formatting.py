5359fb22f77c40738d4b943652ecb66d2b41b2ea
Akmal Hameed <akmal.hameed@hiacuity.com>
2023-06-26 20:03:51 +0530

    All Good

--- utils/text_formatting.py
+++ utils/text_formatting.py
@@ -1,14 +1,19 @@
 def text_formatting(ten_day, three_day, initialized):
-    text_str = "*This is an AUTOMATED MESSAGE* \n\n*New Submissions* \n\n"
-    for assign in initialized:
-        text_str = text_str + f"*{assign['Title']}  {assign['Type']}* \nDue Date - _{assign['Due Date']}_ \nTime Remaining - _{assign['Time Remaining']}_ \n"
+    text_str = "*This is an AUTOMATED MESSAGE* \n"
 
-    text_str = text_str + "\n *Ten Day Reminder* \n\n"
-    for assign in ten_day:
-        text_str = text_str + f"*{assign['Title']}  {assign['Type']}* \nDue Date - {assign['Due Date']} \nTime Remaining - _{assign['Time Remaining']}_ \n"
+    if len(initialized) != 0:
+        "\n * New Submissions * \n\n"
+        for assign in initialized:
+            text_str = text_str + f"*{assign['Title']}  {assign['Type']}* \nDue Date - _{assign['Due Date']}_ \nTime Remaining - _{assign['Time Remaining']}_ \n"
 
-    text_str = text_str + "\n *Three Day Reminder* \n\n"
-    for assign in three_day:
-        text_str = text_str + f"*{assign['Title']}  {assign['Type']}* \nDue Date - _{assign['Due Date']}_ \nTime Remaining - _{assign['Time Remaining']}_ \n"
+    if len(ten_day) != 0:
+        text_str = text_str + "\n *Ten Day Reminder* \n\n"
+        for assign in ten_day:
+            text_str = text_str + f"*{assign['Title']}  {assign['Type']}* \nDue Date - {assign['Due Date']} \nTime Remaining - _{assign['Time Remaining']}_ \n"
+
+    if len(three_day) != 0:
+        text_str = text_str + "\n *Three Day Reminder* \n\n"
+        for assign in three_day:
+            text_str = text_str + f"*{assign['Title']}  {assign['Type']}* \nDue Date - _{assign['Due Date']}_ \nTime Remaining - _{assign['Time Remaining']}_ \n"
 
     return text_str
